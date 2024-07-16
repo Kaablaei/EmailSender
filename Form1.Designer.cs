76021ae1c6c35fa2b92d28e9d3b93f04b1ecf9e4
AmirHossein-Biabani <amirhosseinbiabany@yahoo.com>
2024-07-16 13:27:55 +0330

    Emal Sender

---
+++ Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace EmailSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Send = new System.Windows.Forms.Button();
+            this.Clean = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxEmails = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox104 = new System.Windows.Forms.TextBox();
+            this.textBox105 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 591);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Emil";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 141);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(355, 444);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email Body :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Subject :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Display Name :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(18, 637);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(176, 32);
+            this.Send.TabIndex = 1;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Clean
+            // 
+            this.Clean.Location = new System.Drawing.Point(298, 637);
+            this.Clean.Name = "Clean";
+            this.Clean.Size = new System.Drawing.Size(81, 32);
+            this.Clean.TabIndex = 2;
+            this.Clean.Text = "Clean Statuse";
+            this.Clean.UseVisualStyleBackColor = true;
+            this.Clean.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBoxOutput);
+            this.groupBox2.Controls.Add(this.textBoxEmails);
+            this.groupBox2.Location = new System.Drawing.Point(385, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(575, 629);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Email list to Send";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(415, 16);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.TabIndex = 33;
+            this.label9.Text = "وضعیت";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(114, 16);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "ایمیل ها";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(311, 42);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(239, 573);
+            this.textBoxOutput.TabIndex = 31;
+            // 
+            // textBoxEmails
+            // 
+            this.textBoxEmails.Location = new System.Drawing.Point(24, 42);
+            this.textBoxEmails.Multiline = true;
+            this.textBoxEmails.Name = "textBoxEmails";
+            this.textBoxEmails.Size = new System.Drawing.Size(264, 573);
+            this.textBoxEmails.TabIndex = 30;
+            this.textBoxEmails.TextChanged += new System.EventHandler(this.textBox53_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(64, 686);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 4;
+            // 
+            // textBox104
+            // 
+            this.textBox104.Location = new System.Drawing.Point(171, 10);
+            this.textBox104.Name = "textBox104";
+            this.textBox104.Size = new System.Drawing.Size(253, 20);
+            this.textBox104.TabIndex = 5;
+            this.textBox104.TextChanged += new System.EventHandler(this.textBox104_TextChanged);
+            // 
+            // textBox105
+            // 
+            this.textBox105.Location = new System.Drawing.Point(511, 10);
+            this.textBox105.Name = "textBox105";
+            this.textBox105.Size = new System.Drawing.Size(253, 20);
+            this.textBox105.TabIndex = 6;
+            this.textBox105.TextChanged += new System.EventHandler(this.textBox105_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Email :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(442, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Password :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(579, 686);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(185, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "گروه نرم افزار بیابانی :09390176243 ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 637);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clean Email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1003, 56);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Port: 587 / 465 ";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(993, 82);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(105, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(993, 158);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(105, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged_1);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(999, 135);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(86, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "فاصله بین ارسال ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1006, 623);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 32);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.IndianRed;
+            this.label12.Location = new System.Drawing.Point(1028, 670);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 727);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox105);
+            this.Controls.Add(this.textBox104);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Clean);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Button Clean;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxEmails;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox104;
+        private System.Windows.Forms.TextBox textBox105;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+using System.Threading;
+
+namespace EmailSender
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        string tik = "✔️";
+        string not = "❌";
+        int ZeroEmail = 0;
+        bool cansel = false;
+
+        //
+        //
+        //"amirhoseyn2019@yahoo.com"//""
+
+        private async Task SendEmail(string display, string to, string sub, string body, int port)
+        {
+            var fromAddress = new MailAddress(textBox104.Text.ToString(), display);
+            var toAddress = new MailAddress(to);
+            string fromPassword = textBox105.Text.ToString();
+            string subject = sub;
+            string emailBody = body;
+
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.mail.yahoo.com",
+                // Port = 587,
+                Port = port,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            })
+            {
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = emailBody
+                })
+                {
+                    await smtp.SendMailAsync(message);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+            this.MinimizeBox = true;
+            this.MaximizeBox = true;
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = string.Empty;
+        }
+        bool boltime = false;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            cansel = false;
+            label12.Text = "";
+
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                MessageBox.Show("لطفا دسترسی به اینترنت را چک کنید", " ", MessageBoxButtons.OK);
+                return;
+            }
+
+            string[] emails = textBoxEmails.Text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (emails.Length == 0)
+            {
+                MessageBox.Show("لطفا حداقل یک ایمیل را وارد کنید", " ", MessageBoxButtons.OK);
+
+                return;
+            }
+
+
+
+            int time;
+
+            int port;
+
+            if (!int.TryParse(textBox5.Text, out time) || time < 0)
+            {
+                MessageBox.Show("لطفاً فاصله ارسال را به درستی وارد کنید و مقدار مثبت باشد", "خطا", MessageBoxButtons.OK);
+                boltime = false;
+            }
+            else
+            {
+                time *= 1000; // تبدیل به میلی‌ثانیه
+                boltime = true;
+            }
+
+
+
+
+
+            if (boltime && int.TryParse(textBox4.Text, out port))
+            {
+                label4.Text = "Sending . . . . ";
+                foreach (var email in emails)
+                {
+
+                    try
+                    {
+                        await SendEmail(textBox1.Text, email.Trim(), textBox2.Text, textBox3.Text,port);
+                        textBoxOutput.AppendText($"{email}   ✔️\r\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        ZeroEmail += 1;
+                        textBoxOutput.AppendText($"{email}   ❌\r\n");
+                    }
+                    if (cansel)
+                    {
+                        MessageBox.Show("کنسل شد ", " موفق", MessageBoxButtons.OK);
+
+                        label12.Text = "";
+                        break;
+
+                    }
+
+
+
+                    await Task.Delay(time);
+
+
+
+                }
+
+                if (ZeroEmail == 0)
+                {
+                    MessageBox.Show("تمامی ایمیل ها با موفقیت ارسال شدند", " موفق", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show($"تعداد ایمیل های ارسال نشده : {ZeroEmail} ", " ارور", MessageBoxButtons.RetryCancel);
+                }
+
+
+                label4.Text = "";
+                ZeroEmail = 0;
+                cansel = false;
+            }
+            else
+            {
+                MessageBox.Show("پورت نا معتبر");
+            }
+            boltime = false;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox24_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox47_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox29_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox28_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox36_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox41_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox53_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox104_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox105_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            textBoxEmails.Text = string.Empty;
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            cansel = true;
+            if (boltime)
+            {
+                label12.Text = "Canseling . . . . ";
+            }
+        }
+
+        private void textBox5_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
